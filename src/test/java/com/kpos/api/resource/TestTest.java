b8a8d59b798bff9e5d13fc39a59a44a158634ce1
Kamel Dabwan <kameldabwan@gmail.com>
2016-05-30 23:11:16 -0700

    tests

---
+++ src/test/java/com/kpos/api/resource/TestTest.java
@@ -0,0 +1,16 @@
+package com.kpos.api.resource;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kameldabwan on 5/30/16.
+ */
+public class TestTest {
+    @org.junit.Test
+    public void test1() throws Exception {
+
+    }
+
+}
